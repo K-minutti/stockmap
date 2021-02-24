8c1dbb58aa0b2d5810ad8fe28ceef586bbbc8dea
Kevin Minutti <minutti70@gmail.com>
2021-02-24 17:30:53 -0500

    separating analysis component from home

--- app/components/Routes.js
+++ app/components/Routes.js
@@ -1,17 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./Navbar";
 import Home from "./Home";
 import Analysis from "./Analysis";
+import Dummy from "./Dummy";
 
 const Routes = () => {
   return (
     <Router>
       <Navbar />
       <main>
-        <Home />
-        {/* <Analysis/>
-        <SingleView/> */}
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/analysis" component={Analysis} />
+          <Route exact path="/dummy" component={Dummy} />
+        </Switch>
       </main>
     </Router>
   );
